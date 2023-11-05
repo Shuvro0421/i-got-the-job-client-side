ffd927a3b4bb039d75f781c42c9e935b13dc41f7
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-11-05 22:42:53 +0600

    all jobs page almost done

--- src/components/AllJobs.jsx
+++ src/components/AllJobs.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
+import AllJob from "./AllJob";
 
 
 const AllJobs = () => {
+    const [allJobs, setAllJobs] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/jobs')
+            .then(res => res.json())
+            .then(data => { setAllJobs(data) })
+            .catch(error => console.error('Error fetching jobs:', error));
+    }, []);
 
     return (
         <div>
-            <h1>hello</h1>
+            {
+                allJobs.map((allJob, index) => <AllJob key={index} allJob={allJob}></AllJob>)
+            }
         </div>
     );
 };
