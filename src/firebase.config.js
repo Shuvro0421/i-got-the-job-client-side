18be7e942caf6f1a33ace04298aeaf10c90c22bd
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-11-08 02:15:31 +0600

    firebase auth updated

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB5wypISU-CKxYoPYemVb6bxfSLKEAak4U",
-  authDomain: "i-got-the-job-auth.firebaseapp.com",
-  projectId: "i-got-the-job-auth",
-  storageBucket: "i-got-the-job-auth.appspot.com",
-  messagingSenderId: "754173601299",
-  appId: "1:754173601299:web:8e9ea708f58c1c934b01e6",
-  measurementId: "G-0K79VVB8GM"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
+  measurementId:import.meta.env.VITE_MEASUREMENTID,
 };
 
 // Initialize Firebase
